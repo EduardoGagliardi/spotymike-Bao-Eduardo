f8d5dd7c722897f12bc3788652fdcfb5b7ddf491
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-05-30 13:38:49 +0200

    repair song player

--- src/app/pages/song-player/song-player.page.ts
+++ src/app/pages/song-player/song-player.page.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonToggle, IonButton, IonIcon, IonRange, IonItem, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCol, IonFab, IonFabButton, IonGrid, IonRow, IonButtons } from '@ionic/angular/standalone';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { IonContent, IonTitle, IonButtons, IonButton, IonToolbar, IonHeader, IonRange } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-song-player',
   templateUrl: './song-player.page.html',
   styleUrls: ['./song-player.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonRow, IonGrid, IonFabButton, IonFab, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonRange, IonIcon, IonButton, IonToggle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonRange, IonHeader, IonToolbar, IonButton, IonButtons, IonTitle, IonContent,  
+    CommonModule, 
+    FormsModule,
+  ]
 })
 export class SongPlayerPage implements OnInit {
 
@@ -20,3 +23,6 @@ export class SongPlayerPage implements OnInit {
 
   
 }
+
+
+

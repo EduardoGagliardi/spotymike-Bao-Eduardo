6613252038cd8fb83d11da58b3d80a09b514dfcc
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-05-30 08:47:33 +0200

    create db service

--- src/app/core/interfaces/userTest.ts
+++ src/app/core/interfaces/userTest.ts
@@ -1,30 +1,17 @@
-export interface IUserTest {
+export interface IUser {
+
+  isEmailVerified: boolean;
+  email: string;
+  name: string;
   id: string;
-  name: string;
-  username: string;
-  phone: string;
-  website: string;
-  address?: IAdress;
-  company?: ICompagny;
-  artist?: IArtist;
 }
-export interface IArtist {
-  nbLike: number;
-  year: number;
+
+interface IaccessToken {
+  token: string;
+  expires: string | Date;
 }
-export interface IGeo {
-  lat: string;
-  lng: string;
-}
-export interface IAdress {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo?: IGeo;
-}
-export interface ICompagny {
-  name: string;
-  catchPhrase: string;
-  bs: string;
+
+export interface Itoken {
+  access : IaccessToken;
+  refresh: IaccessToken;
 }
--- src/app/pages/song-player/song-player.page.ts
+++ src/app/pages/song-player/song-player.page.ts
@@ -17,4 +17,5 @@ export class SongPlayerPage implements OnInit {
   ngOnInit() {
   }
 
+  
 }

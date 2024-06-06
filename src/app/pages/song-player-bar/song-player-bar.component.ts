e310a817694a5d7cc4b784acb59648e6a24b8771
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-06-06 20:40:00 +0200

    prepare for merge

--- src/app/pages/song-player-bar/song-player-bar.component.ts
+++ src/app/pages/song-player-bar/song-player-bar.component.ts
@@ -1,20 +1,31 @@
-import { IonContent, IonRange } from '@ionic/angular/standalone';
+import { IonContent, IonRange, IonIcon, IonGrid } from '@ionic/angular/standalone';
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
 
 @Component({
   selector: 'app-song-player-bar',
   templateUrl: './song-player-bar.component.html',
   styleUrls: ['./song-player-bar.component.scss'],
   standalone: true,
-  imports: [IonRange, 
+  imports: [IonGrid, IonIcon, IonRange, 
     IonContent
   ],
 })
 export class SongPlayerBarComponent  implements OnInit {
+  file!: MediaObject;
 
-  constructor() { }
+  constructor(
+    public media: Media,
+    public platform: Platform
+  ){
+  }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
 
+  readAudio() {
+    this.file = this.media.create('../../assets/music/thisIsElonMusk.mp3');
+    this.file.play();
+  }
 }

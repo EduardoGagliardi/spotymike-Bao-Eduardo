0e7d04064a586078305b5884e4f878ccb20c22f3
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-06-07 09:47:29 +0200

    add header for all list and base for the fav card

--- src/app/pages/like/like.page.ts
+++ src/app/pages/like/like.page.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from './../../core/services/local-storage.service';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid } from '@ionic/angular/standalone';
+import { TabsPage } from 'src/app/layouts/tabs/tabs.page';
 
 @Component({
   selector: 'app-like',
   templateUrl: './like.page.html',
   styleUrls: ['./like.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TabsPage],
 })
 export class LikePage implements OnInit {
 
+  LocalStorageService = inject(LocalStorageService);
+  artists = []
+  albums = []
+  songs = []
   constructor() { }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {
-  }
 
+  }
 }

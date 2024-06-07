c58d4193f5fc0a853718b07c0016c0397c86bc08
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-06-07 17:25:50 +0200

    base of playlist card

--- src/app/shared/components/playlist/playlist.component.ts
+++ src/app/shared/components/playlist/playlist.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCol, IonRow, IonIcon } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.scss'],
+  standalone: true,
+  imports: [
+    IonCol,
+    IonRow,
+    IonIcon
+  ]
 })
 export class PlaylistComponent  implements OnInit {
 

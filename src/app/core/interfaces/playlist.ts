9059c14d81bbb4acdfc1e29e4a4f7cdf741b9b51
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-06-07 17:06:02 +0200

    temp

--- src/app/core/interfaces/playlist.ts
+++ src/app/core/interfaces/playlist.ts
@@ -1,4 +1,4 @@
-export interface Playlist {
+export interface IPlaylist {
     _id: string;
     title: string;
     followed: number;
--- src/app/shared/components/playlist/playlist.component.ts
+++ src/app/shared/components/playlist/playlist.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { IonRow, IonCol } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { options, share } from 'ionicons/icons';
+import { IPlaylist } from 'src/app/core/interfaces/playlist';
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.scss'],
+  standalone: true,
+  imports: [
+    IonRow, 
+    IonRow,
+    IonCol,
+
+  ],
 })
 export class PlaylistComponent  implements OnInit {
+  // @Input() Playlist: any;
 
-  constructor() { }
+  Playlist: IPlaylist = {
+    _id: '1',
+    title: 'Ma comande MC Donald',
+    owner: 'Mike',
+    cover: 'assets/music/thisIsElonMusk.png',
+    followed: 20000000000,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    song: [
+      {
+        _id: "1",
+        title: "Spotymike"
+      }
+    ],
+  }
+  constructor() {
+    addIcons({ options });
+    addIcons({ share });
+  }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
 
 }

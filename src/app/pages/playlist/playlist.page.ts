5e6ae7ac518ac8423da538ab120466d12fe15382
EduardoGagliardi <eduardo.gagliardi2003@gmail.com>
2024-06-07 18:14:56 +0200

    j'en ai ma claque

--- src/app/pages/playlist/playlist.page.ts
+++ src/app/pages/playlist/playlist.page.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonText } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.page.html',
   styleUrls: ['./playlist.page.scss'],
   standalone: true,
-  imports: [IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonText, IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PlaylistPage implements OnInit {
 
--- src/app/shared/components/playlist/playlist.component.ts
+++ src/app/shared/components/playlist/playlist.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Playlist } from 'src/app/core/interfaces/playlist';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonCol, IonRow, IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { options, share, heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-playlist',
@@ -12,10 +15,15 @@ import { IonCol, IonRow, IonIcon } from "@ionic/angular/standalone";
     IonIcon
   ]
 })
+
 export class PlaylistComponent  implements OnInit {
+  @Input()Playlist: any;
 
-  constructor() { }
+  constructor() { 
+    addIcons({ options, share, heart });
+  }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
 
 }
